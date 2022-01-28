394607a28cae5690be748236c0af5fae6954eb4e
omiglio <ogalvan5@student.mtsac.edu>
2022-01-28 09:02:27 -0800

    20. Creating The Game Mode

---
+++ Source/SpaceShooter/SpaceShooterGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "SpaceShooterGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPACESHOOTER_API ASpaceShooterGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
