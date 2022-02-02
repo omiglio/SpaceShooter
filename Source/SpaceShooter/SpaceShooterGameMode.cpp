5784130167bcfc3202b6311aa881ea5e93a1fd81
omiglio <ogalvan5@student.mtsac.edu>
2022-02-01 19:51:04 -0800

    21. Spawning Enemies

--- Source/SpaceShooter/SpaceShooterGameMode.cpp
+++ Source/SpaceShooter/SpaceShooterGameMode.cpp
@@ -2,4 +2,36 @@
 
 
 #include "SpaceShooterGameMode.h"
+#include "EnemyController.h"
+
+void ASpaceShooterGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASpaceShooterGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	GameTimer += DeltaTime;
+	EnemyTimer -= DeltaTime;
+
+	if (EnemyTimer <= 0.0f)
+	{
+		float DifficultyPercentage = FMath::Min(GameTimer / 
+			TIME_TO_MINIMUM_INTERVAL, 1.0f);
+		EnemyTimer = MAXIMUM_INTERVAL - (MAXIMUM_INTERVAL -
+			MINIMUM_INTERVAL) * DifficultyPercentage;
+
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			FVector Location = FVector(600.0f, FMath::RandRange
+			(-800.0f, 800.0f), 70.0f);
+
+			World->SpawnActor<AEnemyController>(EnemyBlueprint,
+				Location, FRotator::ZeroRotator);
+		}
+	}
+}
 
--- Source/SpaceShooter/SpaceShooterGameMode.h
+++ Source/SpaceShooter/SpaceShooterGameMode.h
@@ -13,5 +13,23 @@ UCLASS()
 class SPACESHOOTER_API ASpaceShooterGameMode : public AGameMode
 {
 	GENERATED_BODY()
-	
+
+	float MINIMUM_INTERVAL = 0.5f;
+	float MAXIMUM_INTERVAL = 2.0F;
+	float TIME_TO_MINIMUM_INTERVAL = 30.f;
+
+public:
+
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+	UPROPERTY(EditAnywhere, Category = "Spawning")
+	TSubclassOf<class AEnemyController> EnemyBlueprint;
+
+	float EnemyTimer;
+	float GameTimer;
+
+protected:
+
+	int Score = 0;
 };
