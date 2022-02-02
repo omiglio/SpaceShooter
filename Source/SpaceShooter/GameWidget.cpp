02db7ba8997436075709ae732a29ad73ba7a5a2a
omiglio <ogalvan5@student.mtsac.edu>
2022-02-01 21:17:55 -0800

    23. Game Widget

---
+++ Source/SpaceShooter/GameWidget.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "GameWidget.h"
+
+void UGameWidget::Load()
+{
+	const FName TextBlockName = FName(TEXT("GameTextBlock"));
+
+	if (ScoreText == nullptr)
+	{
+		ScoreText = (UTextBlock*) (WidgetTree->FindWidget
+			(TextBlockName));
+	}
+}
+
+void UGameWidget::SetScore(int score)
+{
+	if (ScoreText != nullptr)
+	{
+		ScoreText->SetText(FText::FromString(FString(TEXT("Score: "
+			)) + FString::FromInt(score)));
+	}
+}
+
+void UGameWidget::OnGameOver(int score)
+{
+	if (ScoreText != nullptr)
+	{
+		ScoreText->SetText(FText::FromString(FString(TEXT("Score: "
+		)) + FString::FromInt(score) + TEXT("\nPress R to Restart")));
+	}
+}
